0a5d1dd8027ab90bdd7092c17aee4f1307485bd5
amaryanov <amaryanov@08d4f820-8f9b-11dd-a3d1-51ecd7952c7b>
2010-08-12 17:52:13 +0000

    remove old magento nginx conf

--- trunk/nginx_magento/sites/server/example.com
+++
@@ -1,3 +0,0 @@
-	server_name example.com;
-	access_log /home/amaryanov/build/data/nginx/log/example.com/access.log main;
-	error_log /home/amaryanov/build/data/nginx/log/example.com/error.log;
--- trunk/nmp/conf/php-fpm.conf.sh
+++ trunk/nmp/conf/php-fpm.conf.sh
@@ -1,134 +1,276 @@
 #!/bin/sh
-
 cat <<EOF
-<?xml version="1.0" ?>
-<!--
-start php-fpm: ${phpdir}sbin/php-fpm start
-stop php-fpm: ${phpdir}sbin/php-fpm stop
--->
-<configuration>
+;;;;;;;;;;;;;;;;;;;;;
+; FPM Configuration ;
+;;;;;;;;;;;;;;;;;;;;;
 
-	All relative paths in this config are relative to php's install prefix
+; All relative paths in this configuration file are relative to PHP's install
+; prefix.
 
-	<section name="global_options">
+; Include one or more files. If glob(3) exists, it is used to include a bunch of
+; files from a glob(3) pattern. This directive can be used everywhere in the
+; file.
+;include=
 
-		Pid file
-		<value name="pid_file">${datadir}run/php-fpm.pid</value>
+;;;;;;;;;;;;;;;;;;
+; Global Options ;
+;;;;;;;;;;;;;;;;;;
 
-		Error log file
-		<value name="error_log">${phpdatadir}log/php-fpm.log</value>
+[global]
+; Pid file
+; Default Value: none
+pid = ${datadir}run/php.pid
 
-		Log level
-		<value name="log_level">notice</value>
+; Error log file
+; Default Value: ${phpdir}var/log/php-fpm.log
+error_log = ${phpdatadir}log/php-fpm.log
 
-		When this amount of php processes exited with SIGSEGV or SIGBUS ...
-		<value name="emergency_restart_threshold">10</value>
+; Log level
+; Possible Values: alert, error, warning, notice, debug
+; Default Value: notice
+;log_level = notice
 
-		... in a less than this interval of time, a graceful restart will be initiated.
-		Useful to work around accidental curruptions in accelerator's shared memory.
-		<value name="emergency_restart_interval">1m</value>
+; If this number of child processes exit with SIGSEGV or SIGBUS within the time
+; interval set by emergency_restart_interval then FPM will restart. A value
+; of '0' means 'Off'.
+; Default Value: 0
+;emergency_restart_threshold = 0
 
-		Time limit on waiting child's reaction on signals from master
-		<value name="process_control_timeout">5s</value>
+; Interval of time used by emergency_restart_interval to determine when 
+; a graceful restart will be initiated.  This can be useful to work around
+; accidental corruptions in an accelerator's shared memory.
+; Available Units: s(econds), m(inutes), h(ours), or d(ays)
+; Default Unit: seconds
+; Default Value: 0
+;emergency_restart_interval = 0
 
-		Set to 'no' to debug fpm
-		<value name="daemonize">yes</value>
+; Time limit for child processes to wait for a reaction on signals from master.
+; Available units: s(econds), m(inutes), h(ours), or d(ays)
+; Default Unit: seconds
+; Default Value: 0
+;process_control_timeout = 0
 
-	</section>
+; Send FPM to background. Set to 'no' to keep FPM in foreground for debugging.
+; Default Value: yes
+daemonize = yes
 
-	<workers>
+;;;;;;;;;;;;;;;;;;;;
+; Pool Definitions ; 
+;;;;;;;;;;;;;;;;;;;;
 
-		<section name="pool">
+; Multiple pools of child processes may be started with different listening
+; ports and different management options.  The name of the pool will be
+; used in logs and stats. There is no limitation on the number of pools which
+; FPM can handle. Your system will tell you anyway :)
 
-			Name of pool. Used in logs and stats.
-			<value name="name">default</value>
+; Start a new pool named 'www'.
+[www]
 
-			Address to accept fastcgi requests on.
-			Valid syntax is 'ip.ad.re.ss:port' or just 'port' or '/path/to/unix/socket'
-			<value name="listen_address">${datadir}run/php.sock</value>
+; The address on which to accept FastCGI requests.
+; Valid syntaxes are:
+;   'ip.add.re.ss:port'    - to listen on a TCP socket to a specific address on
+;                            a specific port;
+;   'port'                 - to listen on a TCP socket to all addresses on a
+;                            specific port;
+;   '/path/to/unix/socket' - to listen on a unix socket.
+; Note: This value is mandatory.
+listen = ${datadir}run/php.sock
 
-			<value name="listen_options">
+; Set listen(2) backlog. A value of '-1' means unlimited.
+; Default Value: -1
+;listen.backlog = -1
+ 
+; List of ipv4 addresses of FastCGI clients which are allowed to connect.
+; Equivalent to the FCGI_WEB_SERVER_ADDRS environment variable in the original
+; PHP FCGI (5.2.2+). Makes sense only with a tcp listening socket. Each address
+; must be separated by a comma. If this value is left blank, connections will be
+; accepted from any ip address.
+; Default Value: any
+;listen.allowed_clients = 127.0.0.1
 
-				Set listen(2) backlog
-				<value name="backlog">-1</value>
+; Set permissions for unix socket, if one is used. In Linux, read/write
+; permissions must be set in order to allow connections from a web server. Many
+; BSD-derived systems allow connections regardless of permissions. 
+; Default Values: user and group are set as the running user
+;                 mode is set to 0666
+listen.owner = $(id -nu)
+listen.group = $(id -ng)
+listen.mode = 0600
 
-				Set permissions for unix socket, if one used.
-				In Linux read/write permissions must be set in order to allow connections from web server.
-				Many BSD-derrived systems allow connections regardless of permissions.
-				<value name="owner">$(id -nu)</value>
-				<value name="group">$(id -ng)</value>
-				<value name="mode">0600</value>
-			</value>
+; Unix user/group of processes
+; Note: The user is mandatory. If the group is not set, the default user's group
+;       will be used.
+user = $(id -nu)
+group = $(id -ng)
 
-			Unix user of processes
-			<value name="user">$(id -nu)</value>
+; Choose how the process manager will control the number of child processes.
+; Possible Values:
+;   static  - a fixed number (pm.max_children) of child processes;
+;   dynamic - the number of child processes are set dynamically based on the
+;             following directives:
+;             pm.max_children      - the maximum number of children that can
+;                                    be alive at the same time.
+;             pm.start_servers     - the number of children created on startup.
+;             pm.min_spare_servers - the minimum number of children in 'idle'
+;                                    state (waiting to process). If the number
+;                                    of 'idle' processes is less than this
+;                                    number then some children will be created.
+;             pm.max_spare_servers - the maximum number of children in 'idle'
+;                                    state (waiting to process). If the number
+;                                    of 'idle' processes is greater than this
+;                                    number then some children will be killed.
+; Note: This value is mandatory.
+pm = dynamic
 
-			Unix group of processes
-			<value name="group">$(id -ng)</value>
+; The number of child processes to be created when pm is set to 'static' and the
+; maximum number of child processes to be created when pm is set to 'dynamic'.
+; This value sets the limit on the number of simultaneous requests that will be
+; served. Equivalent to the ApacheMaxClients directive with mpm_prefork.
+; Equivalent to the PHP_FCGI_CHILDREN environment variable in the original PHP
+; CGI.
+; Note: Used when pm is set to either 'static' or 'dynamic'
+; Note: This value is mandatory.
+pm.max_children = 10
 
-			Process manager settings
-			<value name="pm">
+; The number of child processes created on startup.
+; Note: Used only when pm is set to 'dynamic'
+; Default Value: min_spare_servers + (max_spare_servers - min_spare_servers) / 2
+pm.start_servers = 3
 
-				Sets style of controling worker process count.
-				Valid values are 'static' and 'apache-like'
-				<value name="style">static</value>
-			</value>
+; The desired minimum number of idle server processes.
+; Note: Used only when pm is set to 'dynamic'
+; Note: Mandatory when pm is set to 'dynamic'
+pm.min_spare_servers = 3
 
-			The timeout (in seconds) for serving a single request after which the worker process will be terminated
-			Should be used when 'max_execution_time' ini option does not stop script execution for some reason
-			'0s' means 'off'
-			<value name="request_terminate_timeout">0s</value>
+; The desired maximum number of idle server processes.
+; Note: Used only when pm is set to 'dynamic'
+; Note: Mandatory when pm is set to 'dynamic'
+pm.max_spare_servers = 5
+ 
+; The number of requests each child process should execute before respawning.
+; This can be useful to work around memory leaks in 3rd party libraries. For
+; endless request processing specify '0'. Equivalent to PHP_FCGI_MAX_REQUESTS.
+; Default Value: 0
+;pm.max_requests = 500
 
-			The timeout (in seconds) for serving of single request after which a php backtrace will be dumped to slow.log file
-			'0s' means 'off'
-			<value name="request_slowlog_timeout">3s</value>
+; The URI to view the FPM status page. If this value is not set, no URI will be
+; recognized as a status page. By default, the status page shows the following
+; information:
+;   accepted conn    - the number of request accepted by the pool;
+;   pool             - the name of the pool;
+;   process manager  - static or dynamic;
+;   idle processes   - the number of idle processes;
+;   active processes - the number of active processes;
+;   total processes  - the number of idle + active processes.
+; The values of 'idle processes', 'active processes' and 'total processes' are
+; updated each second. The value of 'accepted conn' is updated in real time.
+; Example output:
+;   accepted conn:   12073
+;   pool:             www
+;   process manager:  static
+;   idle processes:   35
+;   active processes: 65
+;   total processes:  100
+; By default the status page output is formatted as text/plain. Passing either
+; 'html' or 'json' as a query string will return the corresponding output
+; syntax. Example:
+;   http://www.foo.bar/status
+;   http://www.foo.bar/status?json
+;   http://www.foo.bar/status?html
+; Note: The value must start with a leading slash (/). The value can be
+;       anything, but it may not be a good idea to use the .php extension or it
+;       may conflict with a real PHP file.
+; Default Value: not set 
+pm.status_path = /phpstatus
+ 
+; The ping URI to call the monitoring page of FPM. If this value is not set, no
+; URI will be recognized as a ping page. This could be used to test from outside
+; that FPM is alive and responding, or to
+; - create a graph of FPM availability (rrd or such);
+; - remove a server from a group if it is not responding (load balancing);
+; - trigger alerts for the operating team (24/7).
+; Note: The value must start with a leading slash (/). The value can be
+;       anything, but it may not be a good idea to use the .php extension or it
+;       may conflict with a real PHP file.
+; Default Value: not set
+ping.path = /phpping
 
-			The log file for slow requests
-			<value name="slowlog">${phpdatadir}log/slow.log</value>
+; This directive may be used to customize the response of a ping request. The
+; response is formatted as text/plain with a 200 response code.
+; Default Value: pong
+ping.response = pong
+ 
+; The timeout for serving a single request after which the worker process will
+; be killed. This option should be used when the 'max_execution_time' ini option
+; does not stop script execution for some reason. A value of '0' means 'off'.
+; Available units: s(econds)(default), m(inutes), h(ours), or d(ays)
+; Default Value: 0
+;request_terminate_timeout = 0
+ 
+; The timeout for serving a single request after which a PHP backtrace will be
+; dumped to the 'slowlog' file. A value of '0s' means 'off'.
+; Available units: s(econds)(default), m(inutes), h(ours), or d(ays)
+; Default Value: 0
+request_slowlog_timeout = 1m
+ 
+; The log file for slow requests
+; Default Value: ${phpdir}var/log/php-fpm.log.slow
+slowlog = ${phpdatadir}log/php-fpm.log.slow
+ 
+; Set open file descriptor rlimit.
+; Default Value: system defined value
+;rlimit_files = 1024
+ 
+; Set max core size rlimit.
+; Possible Values: 'unlimited' or an integer greater or equal to 0
+; Default Value: system defined value
+;rlimit_core = 0
+ 
+; Chroot to this directory at the start. This value must be defined as an
+; absolute path. When this value is not set, chroot is not used.
+; Note: chrooting is a great security feature and should be used whenever 
+;       possible. However, all PHP paths will be relative to the chroot
+;       (error_log, sessions.save_path, ...).
+; Default Value: not set
+;chroot = 
+ 
+; Chdir to this directory at the start. This value must be an absolute path.
+; Default Value: current directory or / when chroot
+;chdir = /var/www
+ 
+; Redirect worker stdout and stderr into main error log. If not set, stdout and
+; stderr will be redirected to /dev/null according to FastCGI specs.
+; Default Value: no
+catch_workers_output = yes
+ 
+; Pass environment variables like LD_LIBRARY_PATH. All $VARIABLEs are taken from
+; the current environment.
+; Default Value: clean env
+env[HOSTNAME] = \$HOSTNAME
+env[PATH] = /usr/local/bin:/usr/bin:/bin
+env[TMP] = ${phpdatadir}tmp
+env[TMPDIR] = ${phpdatadir}tmp
+env[TEMP] = ${phpdatadir}tmp
 
-			Set open file desc rlimit
-			<value name="rlimit_files">1024</value>
+; Additional php.ini defines, specific to this pool of workers. These settings
+; overwrite the values previously defined in the php.ini. The directives are the
+; same as the PHP SAPI:
+;   php_value/php_flag             - you can set classic ini defines which can
+;                                    be overwritten from PHP call 'ini_set'. 
+;   php_admin_value/php_admin_flag - these directives won't be overwritten by
+;                                     PHP call 'ini_set'
+; For php_*flag, valid values are on, off, 1, 0, true, false, yes or no.
 
-			Set max core size rlimit
-			<value name="rlimit_core">0</value>
+; Defining 'extension' will load the corresponding shared extension from
+; extension_dir. Defining 'disable_functions' or 'disable_classes' will not
+; overwrite previously defined php.ini values, but will append the new value
+; instead.
 
-			Chroot to this directory at the start, absolute path
-			<value name="chroot"></value>
-
-			Chdir to this directory at the start, absolute path
-			<value name="chdir"></value>
-
-			Redirect workers' stdout and stderr into main error log.
-			If not set, they will be redirected to /dev/null, according to FastCGI specs
-			<value name="catch_workers_output">yes</value>
-
-			How much requests each process should execute before respawn.
-			Useful to work around memory leaks in 3rd party libraries.
-			For endless request processing please specify 0
-			Equivalent to PHP_FCGI_MAX_REQUESTS
-			<value name="max_requests">200</value>
-
-			Comma separated list of ipv4 addresses of FastCGI clients that allowed to connect.
-			Equivalent to FCGI_WEB_SERVER_ADDRS environment in original php.fcgi (5.2.2+)
-			Makes sense only with AF_INET listening socket.
-			<value name="allowed_clients"></value>
-
-			Pass environment variables like LD_LIBRARY_PATH
-			All \$VARIABLEs are taken from current environment
-			<value name="environment">
-				<value name="HOSTNAME">\$HOSTNAME</value>
-				<value name="PATH">/usr/local/bin:/usr/bin:/bin</value>
-				<value name="TMP">${phpdatadir}tmp</value>
-				<value name="TMPDIR">${phpdatadir}tmp</value>
-				<value name="TEMP">${phpdatadir}tmp</value>
-				<value name="OSTYPE">\$OSTYPE</value>
-				<value name="MACHTYPE">\$MACHTYPE</value>
-				<value name="MALLOC_CHECK_">2</value>
-			</value>
-
-		</section>
-
-	</workers>
-
-</configuration>
+; Default Value: nothing is defined by default except the values in php.ini and
+;                specified at startup with the -d argument
+;php_admin_value[sendmail_path] = /usr/sbin/sendmail -t -i -f www@my.domain.com
+;php_flag[display_errors] = off
+;php_admin_value[error_log] = /var/log/fpm-php.www.log
+;php_admin_flag[log_errors] = on
+;php_admin_value[memory_limit] = 32M
 EOF
