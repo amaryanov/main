ebe10cf906e49aad195a79e82e8f79b7ca9cee2e
amaryanov <amaryanov@08d4f820-8f9b-11dd-a3d1-51ecd7952c7b>
2010-09-23 14:24:21 +0000

    dkim init.d & conf

---
+++ trunk/nmp/conf/dkim/init.d
@@ -0,0 +1,70 @@
+#!/bin/sh
+# dkim-milter daemon start/stop script.
+
+# Usually this is put in /etc/init.d (at least on machines SYSV R4 based
+# systems) and linked to /etc/rc3.d/S99dkim-milter and /etc/rc0.d/K01dkim-milter.
+### BEGIN INIT INFO
+# Provides: dkim-milter
+# Required-Start: $local_fs $network $remote_fs
+# Required-Stop: $local_fs $network $remote_fs
+# Default-Start:  2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: start and stop dkim-milter
+# Description: dkim-milter.
+### END INIT INFO
+dkimmilter=/home/www/server/build/dkim-milter/bin/dkim-filter
+conf=/home/www/server/data/dkim/conf/dkim.conf
+pid=/home/www/server/data/run/dkim.pid
+sock=/home/www/server/data/run/dkim.sock
+
+iffailed()
+{
+	if [ "$?" != "$1" ]
+	then
+		echo " $2"
+		exit 1
+	fi
+}
+
+case "$1" in
+	start)
+		echo -n "Starting dkim-milter "
+		ls $pid 2>/dev/null 1>&2
+		if [ $? = "0" ]
+		then
+			ps -p `cat $pid` -o pid h >/dev/null
+			iffailed 1 " already running"
+		fi
+		$dkimmilter -x $conf
+		iffailed 0 " failed"
+		echo " done"
+	;;
+
+	stop)
+		echo -n "Stoping dkim-milter "
+		ls $pid 2>/dev/null 1>&2
+		iffailed 0 " pid file does not exist"
+		ps -p `cat $pid` -o pid h >/dev/null
+		iffailed 0 " is not running"
+		kill -s TERM `cat $pid`
+		iffailed 0 " failed"
+		rm $pid $sock
+		echo " done"
+	;;
+	reload)
+		echo -n "Stoping dkim-milter "
+		ls $pid 2>/dev/null 1>&2
+		iffailed 0 " pid file does not exist"
+		ps -p `cat $pid` -o pid h >/dev/null
+		iffailed 0 " is not running"
+		kill -s USR1 `cat $pid`
+		iffailed 0 " failed"
+		echo " done"
+	;;
+
+	*)
+		echo "Usage: $0 {start|stop}"
+		exit 1
+	;;
+
+esac
