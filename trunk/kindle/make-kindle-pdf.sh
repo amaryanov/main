ae491468e573640dfcda115513f57b735cf83464
amaryanov@gmail.com <amaryanov@gmail.com@08d4f820-8f9b-11dd-a3d1-51ecd7952c7b>
2011-07-02 16:39:15 +0000

    add kindle pdf maker. which removes blank margins and resizes image pdfs

---
+++ trunk/kindle/make-kindle-pdf.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+pdf="$1"
+tempimagename="`mktemp`"
+tempimage="${tempimagename}-000.pbm"
+temprespdf="${tempimage}.pdf"
+touch "$tempimage"
+temppdf="`mktemp`.pdf"
+temppdf2="`mktemp`"
+md5file="`mktemp`"
+md5sum "$tempimage" > $md5file
+i="1"
+
+while true
+do
+	echo -n "Processing page $i..."
+	#convert -density 300 "$pdf"[$i] -resize 100% "$tempimage"
+	pdfimages -f $i -l $i "$pdf" "$tempimagename"
+	md5sum -c --status $md5file
+	if [ "$?" = "0" ]
+	then
+		break
+	fi
+	md5sum "$tempimage" > $md5file
+	convert "$tempimage" -crop `convert "$tempimage" -virtual-pixel edge -blur 0x13 -fuzz 13% -trim -format '%wx%h%O' info:` +repage \
+		-quality 100% -type Grayscale -resize 750x -density 167 -compress jpeg "$temprespdf"
+	if [ "$?" = "0" ]
+	then
+		if [ "$i" = "1" ]
+		then
+			mv "$temprespdf" "$temppdf"
+		else
+			pdftk "$temppdf" "$temprespdf" cat output "$temppdf2"
+			mv "$temppdf2" "$temppdf"
+		fi
+	else
+		echo "error"
+		break
+	fi
+	echo "DONE"
+	i=`expr $i + 1`
+done
+rm -f "$tempimage"
+rm -f "$temprespdf"
+rm -f "$temppdf2"
+rm -f "$tempimagename"
+rm -f "$md5file"
+echo "Result file is: $temppdf"
