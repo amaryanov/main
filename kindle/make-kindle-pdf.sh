00d4e0d4d8db2f678eb0fa95c1dd0e04e8126402
amaryanov <amaryanov@gmail.com>
2011-07-02 19:52:07 +0000

    fix for ppm

--- kindle/make-kindle-pdf.sh
+++ kindle/make-kindle-pdf.sh
@@ -1,32 +1,45 @@
 #!/bin/sh
 pdf="$1"
 tempimagename="`mktemp`"
-tempimage="${tempimagename}-000.pbm"
-temprespdf="${tempimage}.pdf"
-touch "$tempimage"
+tempimagepbm="${tempimagename}-000.pbm"
+tempimageppm="${tempimagename}-000.ppm"
+temprespdf="${tempimagename}.pdf"
 temppdf="`mktemp`.pdf"
 temppdf2="`mktemp`"
-md5file="`mktemp`"
 pages=`pdfinfo "$pdf" | grep -i pages | sed 's/.*\s\([0-9]\+\)$/\1/'`
-md5sum "$tempimage" > $md5file
-i="1"
+start="511"
+i="$start"
+
+rmTemp()
+{
+	rm -f "$tempimagepbm"
+	rm -f "$tempimageppm"
+	rm -f "$temprespdf"
+	rm -f "$temppdf2"
+	rm -f "$tempimagename"
+}
+
+rmTemp
 
 while [ $i -le $pages ]
 do
 	echo -n "Processing page $i..."
-	#convert -density 300 "$pdf"[$i] -resize 100% "$tempimage"
 	pdfimages -f $i -l $i "$pdf" "$tempimagename"
-	md5sum -c --status $md5file
-	if [ "$?" = "0" ]
+	if [ -e $tempimagepbm ]
 	then
+		tempimage="$tempimagepbm"
+	elif [ -e $tempimageppm ]
+	then
+		tempimage="$tempimageppm"
+	else
+		echo "no file"
 		break
 	fi
-	md5sum "$tempimage" > $md5file
 	convert "$tempimage" -crop `convert "$tempimage" -virtual-pixel edge -blur 0x13 -fuzz 13% -trim -format '%wx%h%O' info:` +repage \
 		-quality 100% -resize 750x -density 167 -compress lossless "$temprespdf"
 	if [ "$?" = "0" ]
 	then
-		if [ "$i" = "1" ]
+		if [ "$i" = "$start" ]
 		then
 			mv "$temprespdf" "$temppdf"
 		else
@@ -38,11 +51,8 @@ do
 		break
 	fi
 	echo "DONE"
+	rmTemp
 	i=`expr $i + 1`
 done
-rm -f "$tempimage"
-rm -f "$temprespdf"
-rm -f "$temppdf2"
-rm -f "$tempimagename"
-rm -f "$md5file"
+rmTemp
 echo "Result file is: $temppdf"
